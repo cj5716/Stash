633732852da3aaefee05e55f0c5d0767e94aa5de
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-05 10:17:50 +0800

    Bench 7421093

--- src/sources/search.c
+++ src/sources/search.c
@@ -628,7 +628,7 @@ __main_loop:
                 R = iclamp(R, 0, newDepth - 1);
             }
             else
-                R = 1 + !pvNode;
+                R = 1 - givesCheck;
         }
         else
             R = 0;
