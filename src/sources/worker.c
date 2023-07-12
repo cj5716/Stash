71af5e7744499f299543c7e80b7267e1c6afebb4
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-12 10:31:28 +0800

    Bench 8412352

--- src/sources/worker.c
+++ src/sources/worker.c
@@ -259,7 +259,7 @@ void wpool_start_search(WorkerPool *wpool, const Board *rootBoard, const SearchP
 
             curRootMove->move = UciSearchMoves.moves[k].move;
             curRootMove->seldepth = 0;
-            curRootMove->score = curRootMove->prevScore = -INF_SCORE;
+            curRootMove->score = curRootMove->prevScore = curRootMove->avgScore = -INF_SCORE;
             curRootMove->pv[0] = curRootMove->pv[1] = NO_MOVE;
         }
     }
