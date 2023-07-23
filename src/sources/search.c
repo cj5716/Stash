34ccbde3d55daa26c565e9f36189da5ec5a99853
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-23 13:55:40 +0800

    Bench 8464901

--- src/sources/search.c
+++ src/sources/search.c
@@ -499,7 +499,16 @@ score_t search(bool pvNode, Board *board, int depth, score_t alpha, score_t beta
     }
 
     // Reduce depth if the node is absent from TT.
-    if (!rootNode && !found && depth >= 4) --depth;
+    if (!ss->excludedMove && !rootNode && !found && depth >= 4) --depth;
+
+    // Reduce depth if the we are on the PV, and the TT entry is at a much higher depth but has no TT move.
+    // Drop into qsearch if depth <= 0.
+    if (pvNode && found && ttMove == NO_MOVE && ttDepth >= depth + 3) 
+    {
+        --depth;
+        if (depth <= 0)
+            return qsearch(true, board, alpha, beta, ss);
+    }
 
 __main_loop:
     movepicker_init(&mp, false, board, worker, ttMove, ss);
