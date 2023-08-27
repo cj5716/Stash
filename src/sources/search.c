27bb178e6780661db1a1400bd6506e021de9dced
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-27 12:32:52 +0800

    Bench 7545914

--- src/sources/search.c
+++ src/sources/search.c
@@ -506,6 +506,8 @@ score_t search(bool pvNode, Board *board, int depth, score_t alpha, score_t beta
     // Reduce depth if the node is absent from TT.
     if (!rootNode && !found && depth >= 4) --depth;
 
+    if (pvNode && found && ttDepth >= depth && ttMove == NO_MOVE && depth >= 8) --depth;
+
 __main_loop:
     movepicker_init(&mp, false, board, worker, ttMove, ss);
 
