2a7060a1db8ad323dfa1d7dacd5f95cadeff3ec7
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-01 12:19:08 +0800

    Bench 6680171

--- src/include/history.h
+++ src/include/history.h
@@ -38,7 +38,7 @@ typedef move_t countermove_history_t[PIECE_NB][SQUARE_NB];
 // Returns the history bonus for the given depth.
 INLINED int history_bonus(int depth)
 {
-    return imin(16 * depth * depth, 2048);
+    return imin(18 * depth * depth, 2048);
 }
 
 // Updates the butterfly history table for the given piece and move.
