77ec4933526b6fab57a204eea59b9b1a1440215c
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-03 15:02:31 +0800

    Bench 6025272

--- src/sources/search.c
+++ src/sources/search.c
@@ -34,7 +34,7 @@ int Pruning[2][16];
 void init_search_tables(void)
 {
     // Compute the LMR base values.
-    for (int i = 1; i < 256; ++i) 
+    for (int i = 1; i < 256; ++i)
     {
         Reductions[0][i] = (int)(log(i) * 11.17 + 4.21); // Noisy LMR formula
         Reductions[1][i] = (int)(log(i) * 23.12 + 8.20); // Quiet LMR formula
@@ -50,7 +50,8 @@ void init_search_tables(void)
 
 int lmr_base_value(int depth, int movecount, bool improving, bool isQuiet)
 {
-    return (-685 + Reductions[isQuiet][depth] * Reductions[isQuiet][movecount] + !improving * 416) / 1024;
+    return (-685 + Reductions[isQuiet][depth] * Reductions[isQuiet][movecount] + !improving * 416)
+           / 1024;
 }
 
 void init_searchstack(Searchstack *ss)
@@ -795,7 +796,7 @@ __main_loop:
                 {
                     // Update move histories.
                     if (isQuiet) update_quiet_history(board, depth, bestmove, quiets, qcount, ss);
-                    if (moveCount != 1)
+                    if (moveCount != 1 || depth > 6)
                         update_capture_history(board, depth, bestmove, captures, ccount, ss);
                     break;
                 }
