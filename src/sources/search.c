e79957df65d46407a54b96dffb97a7b03e60c711
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-23 10:27:57 +0800

    Bench 5913071

--- src/sources/search.c
+++ src/sources/search.c
@@ -797,7 +797,8 @@ __main_loop:
                 {
                     // Update move histories.
                     if (isQuiet) update_quiet_history(board, depth, bestmove, quiets, qcount, ss);
-                    update_capture_history(board, depth, bestmove, captures, ccount, ss);
+                    if (moveCount != 1)
+                        update_capture_history(board, depth, bestmove, captures, ccount, ss);
                     break;
                 }
             }
